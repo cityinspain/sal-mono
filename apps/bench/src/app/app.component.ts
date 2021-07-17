0d9254cd0c167bf1fbea1b2564ff615682ba79a0
Anthony Salamanca <salamanca@edataquest.com>
2021-07-17 10:54:03 -0400

    initial commit

---
+++ apps/bench/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sal-mono-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'bench';
+}
